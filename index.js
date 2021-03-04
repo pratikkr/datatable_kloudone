874f86a744c56eabc6506f2fbefa802e2d81eb6b
pratik kumar <kumarpratik861@gmail.com>
2021-03-04 15:03:12 +0530

    added selection of api

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-fetch("https://raw.githubusercontent.com/accuknox/TrainingAPI/main/small.json")
-.then(
-    res => {
-        res.json().then(
-            data =>{
-                console.log(data);
-                if(data.length > 0){
-                    var temp ="";
-
-                    data.forEach((u) => {
-                        temp+= "<tr>";
-                        temp+= "<td>" + u.id+"</td>";
-                        temp+= "<td>" + u.firstName+"</td>";
-                        temp+= "<td>" + u.lastName+"</td>";
-                        temp+= "<td>" + u.location+"</td>";
-                        temp+= "<td>" + u.date+"</td>";
-                        temp+= "<td>" + u.salary+"</td></tr>";
-                    });
-                    document.getElementById("table").innerHTML =temp;
-                }
-            })
-
+function change(){
+    let url;
+    var update;
+    if(document.getElementById("select").value == "small"){
+        url = "https://raw.githubusercontent.com/accuknox/TrainingAPI/main/small.json";
     }
-)
+    else if(document.getElementById("select").value == "medium"){
+        url = "https://raw.githubusercontent.com/accuknox/TrainingAPI/main/medium.json";
+    }
+    else if(document.getElementById("select").value == "large"){
+        url = "https://raw.githubusercontent.com/accuknox/TrainingAPI/main/large.json";
+    }
+    fetch(url)
+        .then(
+        res => {
+            res.json().then(
+                data =>{
+                    console.log(data);
+                    if(data.length > 0){
+                        var temp ="";
+    
+                        data.forEach((u) => {
+                            temp+= "<tr>";
+                            temp+= "<td>" + u.id+"</td>";
+                            temp+= "<td>" + u.firstName +" " + u.lastName+"</td>";
+                            // temp+= "<td>" + u.lastName+"</td>";
+                            temp+= "<td>" + u.location+"</td>";
+                            temp+= "<td>" + u.date+"</td>";
+                            temp+= "<td>" + u.salary+"</td></tr>";
+                        });
+                        document.getElementById("table").innerHTML =temp;
+    
+                        var count = data.length();
+                        console.log(count);
+                    }
+                })
+    
+        }
+        )
+}
 
-// const searchFn =()=>{
-//     let filter = document.getElementById('search').nodeValue;
-//     let myTable = document.getElementById('table');
-//     let tr =table.getElementsByTagName('tr');
-
-//     for(var i =0 ; i< tr.length ; i++){
-//         let td = tr[i].getElementsByTagName('td')[1];
-
-//         if(td){
-//             let textvalue =td.textcontent || td.innerHTML;
-//             if(textvalue.indexOf())
-//         }
-
-//     }
-
-
-// }
